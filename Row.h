ea74afcb820cbc5c2545b6d233b5d1d52d08ed9b
Nahoa <rraymond001@gmail.com>
2017-01-15 20:18:25 -0800

    Update Row.h
    1/15/17
    updated class

--- Row.h
+++ Row.h
@@ -1,18 +1,17 @@
 #pragma once
-#include"Array2D.h"
+#include "Array2D.h"
 
-template<typename T>
-class Row 
+template <typename T>
+class Row
 {
 public:
 	Row();
 	Row(Array2D& array, int row);
-	Row(const Row & copy);//copy ctor
-	T & operator[] (int column) const;
+	Row(const Row & copy);
+	Row& operator=(const Row& rhs);
+	T & operator[](int column);
 	~Row();
-
 private:
 	Array2D& m_array2D;
 	int m_row;
 };
-
